a750c180ff6141708893e5fe4c4718d0ef21e51d
germo <germo@LAPTOP-UP6DGQ4Q>
2021-12-24 22:13:51 -0500

    Add project files.

---
+++ AutoClickerApp.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace AutoClicker
+{
+    partial class AutoClickerApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Timer = new System.Windows.Forms.Timer(this.components);
+            this.BtnStart = new System.Windows.Forms.Button();
+            this.IntervalBox = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TrueOrFalseLabel = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.CounterLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tableLayoutPanel2 = new System.Windows.Forms.TableLayoutPanel();
+            this.tableLayoutPanel3 = new System.Windows.Forms.TableLayoutPanel();
+            ((System.ComponentModel.ISupportInitialize)(this.IntervalBox)).BeginInit();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel2.SuspendLayout();
+            this.tableLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Timer
+            // 
+            this.Timer.Tick += new System.EventHandler(this.Timer_Tick);
+            // 
+            // BtnStart
+            // 
+            this.BtnStart.BackColor = System.Drawing.Color.RoyalBlue;
+            this.BtnStart.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.BtnStart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnStart.Location = new System.Drawing.Point(278, 7);
+            this.BtnStart.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.BtnStart.Name = "BtnStart";
+            this.BtnStart.Size = new System.Drawing.Size(254, 89);
+            this.BtnStart.TabIndex = 3;
+            this.BtnStart.TabStop = false;
+            this.BtnStart.Text = "Start (F6)";
+            this.BtnStart.UseVisualStyleBackColor = false;
+            this.BtnStart.Click += new System.EventHandler(this.BtnStart_Click);
+            // 
+            // IntervalBox
+            // 
+            this.IntervalBox.Location = new System.Drawing.Point(8, 58);
+            this.IntervalBox.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.IntervalBox.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.IntervalBox.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.IntervalBox.Name = "IntervalBox";
+            this.IntervalBox.Size = new System.Drawing.Size(248, 35);
+            this.IntervalBox.TabIndex = 4;
+            this.IntervalBox.TabStop = false;
+            this.IntervalBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.IntervalBox.Value = new decimal(new int[] {
+            100,
+            0,
+            0,
+            0});
+            // 
+            // label3
+            // 
+            this.label3.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(28, 12);
+            this.label3.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(213, 29);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Clicker Is Running:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // TrueOrFalseLabel
+            // 
+            this.TrueOrFalseLabel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.TrueOrFalseLabel.AutoSize = true;
+            this.TrueOrFalseLabel.Location = new System.Drawing.Point(278, 12);
+            this.TrueOrFalseLabel.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.TrueOrFalseLabel.Name = "TrueOrFalseLabel";
+            this.TrueOrFalseLabel.Size = new System.Drawing.Size(73, 29);
+            this.TrueOrFalseLabel.TabIndex = 6;
+            this.TrueOrFalseLabel.Text = "False";
+            this.TrueOrFalseLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(8, 11);
+            this.label5.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(247, 29);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Interval (milliseconds)";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.CounterLabel, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.label3, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.TrueOrFalseLabel, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(13, 14);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(4);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(540, 107);
+            this.tableLayoutPanel1.TabIndex = 8;
+            // 
+            // CounterLabel
+            // 
+            this.CounterLabel.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.CounterLabel.AutoSize = true;
+            this.CounterLabel.Location = new System.Drawing.Point(278, 65);
+            this.CounterLabel.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.CounterLabel.Name = "CounterLabel";
+            this.CounterLabel.Size = new System.Drawing.Size(26, 29);
+            this.CounterLabel.TabIndex = 9;
+            this.CounterLabel.Text = "0";
+            this.CounterLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 65);
+            this.label1.Margin = new System.Windows.Forms.Padding(8, 0, 8, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(208, 29);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Number Of Clicks:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tableLayoutPanel2
+            // 
+            this.tableLayoutPanel2.ColumnCount = 2;
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Controls.Add(this.BtnStart, 1, 0);
+            this.tableLayoutPanel2.Controls.Add(this.tableLayoutPanel3, 0, 0);
+            this.tableLayoutPanel2.Location = new System.Drawing.Point(13, 129);
+            this.tableLayoutPanel2.Margin = new System.Windows.Forms.Padding(4);
+            this.tableLayoutPanel2.Name = "tableLayoutPanel2";
+            this.tableLayoutPanel2.RowCount = 1;
+            this.tableLayoutPanel2.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel2.Size = new System.Drawing.Size(540, 109);
+            this.tableLayoutPanel2.TabIndex = 9;
+            // 
+            // tableLayoutPanel3
+            // 
+            this.tableLayoutPanel3.ColumnCount = 1;
+            this.tableLayoutPanel3.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Controls.Add(this.label5, 0, 0);
+            this.tableLayoutPanel3.Controls.Add(this.IntervalBox, 0, 1);
+            this.tableLayoutPanel3.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel3.Name = "tableLayoutPanel3";
+            this.tableLayoutPanel3.RowCount = 2;
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel3.Size = new System.Drawing.Size(264, 103);
+            this.tableLayoutPanel3.TabIndex = 4;
+            // 
+            // AutoClickerApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 29F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(566, 251);
+            this.Controls.Add(this.tableLayoutPanel2);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F);
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.KeyPreview = true;
+            this.Margin = new System.Windows.Forms.Padding(8, 7, 8, 7);
+            this.Name = "AutoClickerApp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Auto Clicker App";
+            this.TopMost = true;
+            ((System.ComponentModel.ISupportInitialize)(this.IntervalBox)).EndInit();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel2.ResumeLayout(false);
+            this.tableLayoutPanel3.ResumeLayout(false);
+            this.tableLayoutPanel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer Timer;
+        private System.Windows.Forms.Button BtnStart;
+        private System.Windows.Forms.NumericUpDown IntervalBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label TrueOrFalseLabel;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Label CounterLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel2;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel3;
+    }
+}
+
---
+++ AutoClickerApp.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+
+namespace AutoClicker
+{
+    public partial class AutoClickerApp : Form
+    {
+        public AutoClickerApp()
+        {
+            InitializeComponent();
+
+            RegisterHotKey(this.Handle, mActionHotKeyID, 0, (int)Keys.F6);
+        }
+        // -------------- this allows for global keyboard presses --------------
+        const int mActionHotKeyID = 1;
+
+        [DllImport("user32.dll")]
+        public static extern bool RegisterHotKey(IntPtr hWnd, int id, int fsModifiers, int key);
+        [DllImport("user32.dll")]
+        public static extern bool UnregisterHotKey(IntPtr hwnd, int id);
+
+        protected override void WndProc(ref Message m)
+        {
+            if (m.Msg == 0x0312 && m.WParam.ToInt32() == mActionHotKeyID)
+            {
+                StartClicker();
+            }
+            base.WndProc(ref m);
+        }
+
+        // -------------- this allows for setting cursor position and cursor clicks --------------
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        //Mouse actions
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
+        private int clickCount = 0;
+
+        // start button
+        private void BtnStart_Click(object sender, EventArgs e) => StartClicker();
+
+        // toggle the timer and set interval value
+        private void StartClicker()
+        {
+            int interval = (int)IntervalBox.Value;
+
+            if (Timer.Enabled == false)
+            {
+                Timer.Enabled = true;
+                Timer.Interval = interval;
+                TrueOrFalseLabel.Text = "True";
+                BtnStart.Text = "Stop (F6)";
+            }
+            else
+            {
+                Timer.Enabled = false;
+                TrueOrFalseLabel.Text = "False";
+                BtnStart.Text = "Start (F6)";
+            }
+        }
+
+        // this is called every timer tick
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            DoMouseClick();
+            clickCount++;
+            CounterLabel.Text = clickCount.ToString();
+        }
+
+        // this set the current cursor position and calls mouse_event()
+        public void DoMouseClick()
+        {
+            //Call the imported function with the cursor's current position            
+            uint Y = (uint)Cursor.Position.Y;
+            uint X = (uint)Cursor.Position.X;
+            mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+        }
+
+        // NOTES: study how this is working and get a good idea before continuing
+        // Future plans?
+        // - allow for right OR left click?
+        // - move the x and y a small amount randomly to prevent detection
+        // - allow for setting a certain number of clicks OR click until stop
+
+    }
+}
